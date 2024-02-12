f2671a8f5ff90c97ca34c7bd8ca23a3f22778b0e
JairoTinga <158531784+JairoTinga@users.noreply.github.com>
2024-02-12 16:25:09 +0800

    Add files via upload

--- Jairo/app.js
+++ Jairo/app.js
@@ -1,43 +1,20 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-
-    
     const productListcontainer = document.getElementById('productList');
-
 
     data.forEach(product => {
       const listItem = document.createElement('p');
-   listItem.textContent='${product.product_name}';
+      listItem.textContent = `
+        Name: ${product.Name}
+        Description: ${product.Description}
+        Price: ${product.Price}
+        Date Added: ${product.DateAdded}
+      `;
       productListcontainer.appendChild(listItem);
     });
 
   })
-  
   .catch(error => {
-  console.error('Error fetching data;', error);
+    console.error('Error fetching data:', error);
   });
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
